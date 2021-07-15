bc9593bead55c40f0ba42bd59e75184525fb8071
Jeremias Jalil <jjejalil@gmail.com>
2021-07-14 21:35:33 -0300

    corrección abouth

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,7 +7,7 @@ export default function About() {
         <div className={style.div}>
             <img className={style.img} src='https://assets.soyhenry.com/henry-landing/assets/Henry/logo-white.png' />
             <div className={style.content}><h1>Practice app</h1>
-                <h2>This is a development by Jeremías Jalil, used as a practice of the concepts acquired on Reac, Redux, Route, API conection, fornt persistence and CSS module, in the Full Stack Web Developer Bootcamp</h2>
+                <h2>This is a development by Jeremías Jalil, used as a practice of the concepts acquired on React, Redux, Route, API connection, fornt persistence and CSS module, in the Full Stack Web Developer Bootcamp</h2>
                 <a className={style.a} href='https://www.linkedin.com/in/jeremiasjalil/' target='_blank'>Jeremías Jalil LinkedIn</a>
             </div>
         </div>
